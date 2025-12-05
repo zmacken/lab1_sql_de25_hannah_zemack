797acf36c72d88631bbcd24ccc2d330f047a3c85
Hannah Zemack <hannah.zemack@gmail.com>
2025-12-05 12:00:39 +0100

    follow kokchuns tutorial for dlt

---
+++ load_sakila_sqlite_duckdb.py
@@ -0,0 +1,19 @@
+#from kokchungs video code along
+import dlt
+from dlt.sources.sql_database import sql_database
+from pathlib import Path
+
+SQLITE_PATH = Path(__file__).parent / "sqlite-sakila.db"
+DUCKDB_PATH = Path(__file__).parent / "sakila.duckdb"
+
+source = sql_database(credentials=f"sqlite:///{SQLITE_PATH}", schema="main")
+
+pipeline = dlt.pipeline(
+    pipeline_name="sakila_sqlite_to_duckdb",
+    destination=dlt.destinations.duckdb(str(DUCKDB_PATH)),
+    dataset_name="staging",
+)
+
+load_info = pipeline.run(source, write_disposition="replace")
+
+print(load_info)
